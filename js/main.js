0743cb544517d34446e9bff6a69a0e85114dabd5
Fedor Božík <fedor.bozik@buckleup.sk>
2020-02-12 15:29:35 +0100

    Update major redesign

--- js/main.js
+++ js/main.js
@@ -1,56 +1,41 @@
-/*AOS init*/
+// AOS init
 AOS.init();
 
-/*smooth scroll initialization*/
-var scroll = new SmoothScroll('a[href*="#"]');
-
-
-/* BACKGROUND VIDEO JS SOURCE */
-var vid = document.getElementById("bgvid");
-var pauseButton = document.querySelector("#bgvid");
-
-if (window.matchMedia('(prefers-reduced-motion)').matches) {
-    vid.removeAttribute("autoplay");
-    vid.pause();
-}
-
-function vidFade() {
-  vid.classList.add("stopfade");
-}
-
-vid.addEventListener('ended', function()
-{
-// only functional if "loop" is removed
-vid.pause();
-// to capture IE10
-vidFade();
-});
-
-
-pauseButton.addEventListener("click", function() {
-  if (vid.paused) {
-    vid.play();
-    // disabled because it causes the fixed social icons to go crazy
-    //vid.classList.toggle("stopfade");
-  } else {
-    vid.pause();
-    //vid.classList.toggle("stopfade");
-  }
-});
-
 // CONTACT FORM STUFF
-function processSubmitEmailRequest(){
-    var address = "fedor.bozik@outlook.com";
-    var subject = document.getElementById("subject").value;
-    var body = document.getElementById("message").value;
+function processSubmitEmailRequest() {
+    var address = 'fedor.bozik@outlook.com';
+    var subject = document.getElementById('subject').value;
+    var body = document.getElementById('message').value;
 
     var url = 'mailto:' + address + '?subject=' + subject + '&body=' + body;
     window.open(url, "_blank")
 };
 
+function createGameIframe (iframePopup) {
+    let iframe = document.createElement('iframe');
+    iframe.frameborder = '0';
+    iframe.src = 'https://codepen.io/bozik/embed/wjdKKM?height=600&theme-id=dark&default-tab=result';
+    iframePopup.prepend(iframe);
+};
 
+function removeIframe () {
+    $("#iframe-popup iframe").remove();
+};
 
+$( document ).ready(function() {
+    console.log( "ready!" );
 
+    let iframePopup = $("#iframe-popup");
 
+    // Game popup handler
+    $("#game").click(function () {
+        iframePopup.fadeIn(200);
+        createGameIframe(iframePopup);
+    });
 
+    $(".close").click(function () {
+        iframePopup.fadeOut(200);
+        removeIframe();
+    });
 
+});
