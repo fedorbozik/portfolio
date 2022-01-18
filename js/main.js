b2ded9ee724ac7f8766961c8f856537719acdf1a
Fedor Božík <bzk.333+github@gmail.com>
2022-01-18 14:45:13 -0500

    Rm HJ trkg

--- js/main.js
+++ js/main.js
@@ -84,20 +84,6 @@ function switchTheme() {
 };
 
 $(document).ready(function() {
-    (function (h, o, t, j, a, r) {
-        h.hj =
-            h.hj ||
-            function () {
-                (h.hj.q = h.hj.q || []).push(arguments);
-            };
-        h._hjSettings = {hjid: 1238533, hjsv: 6};
-        a = o.getElementsByTagName("head")[0];
-        r = o.createElement("script");
-        r.async = 1;
-        r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
-
     let iframePopup = $("#iframe-popup");
     let overlay = $("#iframe-overlay");
 
